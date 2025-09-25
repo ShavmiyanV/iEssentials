21a3b46c1ed5455f12cf2a4b81f7a2ddda2f4a0b
Shavmiyan_V <168546643+ShavmiyanV@users.noreply.github.com>
2025-09-25 23:53:36 +0530

    Add Container component and update Header layout
    Introduced a reusable Container component for consistent page layout. Updated the Header component to use Container and added structure for logo, navigation, and admin navigation sections.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Container from './Container'
 
 const Header = () => {
   return (
-    <header>Header</header>
+    <header className='bg-white py-5'>
+      <Container>
+        {/* Logo*/}
+        {/* NavBar*/}
+        {/* NavAdmin*/}
+      </Container>
+    </header>
   )
 }
 
