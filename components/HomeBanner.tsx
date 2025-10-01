fbb9a548f33b20d1d7f566704b7ecb8e8f45ccff
Shavmiyan_V <168546643+ShavmiyanV@users.noreply.github.com>
2025-10-01 18:41:46 +0530

    Add text styling page

--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Title } from "./ui/text";
 
 const HomeBanner = () => {
   return (
     <div className="py-16 md:py-0 bg-shop-light-pink rounded-lg px-10 lg:px-24 flex items-center justify-between">
-      HomeBanner
+      <div>
+        <Title>
+          Grab Upto 50% off on <br />
+          Selected Headphone
+        </Title>
+      </div>
+      <div></div>
     </div>
   );
 };
---
+++ components/ui/text.tsx
@@ -0,0 +1,13 @@
+import { cn } from "@/lib/utils";
+
+export const Title = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <h2 className={cn("text-2xl font-semibold", className)}>{children}</h2>
+  );
+};
