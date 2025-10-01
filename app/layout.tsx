ade6bb64da711f453d0f5c38cd993633e1814962
Shavmiyan_V <168546643+ShavmiyanV@users.noreply.github.com>
2025-10-01 18:25:09 +0530

    Add Container for Footer and Fix Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,11 @@ import Footer from "@/components/Footer";
 import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
-  title: "iEssentials",
-  description: "Largest Store For Apple Products",
+  title: {
+    template: "%s - Shpocart online store",
+    default: "Shopcart online store",
+  },
+  description: "Shopcart online store, Your one stop shop for all your needs",
 };
 
 export default function RootLayout({
@@ -20,10 +23,8 @@ export default function RootLayout({
         <body className="font-poppins antialiased">
           <div className="flex flex-col min-h-screen">
             <Header />
-          <main className="flex-1">
-            {children}
-          </main>
-          <Footer />
+            <main className="flex-1">{children}</main>
+            <Footer />
           </div>
         </body>
       </html>
