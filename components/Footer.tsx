85f13ce8ea68ff1e87fc0c30f4be4e27adc0e046
Shavmiyan_V <168546643+ShavmiyanV@users.noreply.github.com>
2025-10-02 21:46:00 +0530

    Update Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import Container from "./Container";
 import FooterTop from "./FooterTop";
+import Logo from "./Logo";
 
 const Footer = () => {
   return (
-    <footer className="bg-white border-t" >
+    <footer className="bg-white border-t">
       <Container>
-        <FooterTop/>
+        <FooterTop />
+        <div>
+          <div>
+            <Logo />
+            <p>
+              Discover curated furniture collections at Shopcart, blending style
+              and comfort to elevate your living spaces.
+            </p>
+          </div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </Container>
     </footer>
   );
