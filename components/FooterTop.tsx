61348a216f19ba7da740296014c3f3efbe28778a
Shavmiyan_V <168546643+ShavmiyanV@users.noreply.github.com>
2025-10-02 21:11:37 +0530

    Update FooterTop.tsx

--- components/FooterTop.tsx
+++ components/FooterTop.tsx
@@ -1,4 +1,23 @@
+import { MapPin } from 'lucide-react';
 import React from 'react'
+
+
+interface ContactItemData {
+  title: string;
+  subtitle: string;
+  icon: React.ReactNode;
+}
+
+const data: ContactItemData[] = [
+  {
+    title: "Visit Us",
+    subtitle: "New Orlean, USA",
+    icon: (
+      <MapPin className='h-6 w-6 text-gray-600 group-hover:text-primary transition-colors'/>
+    ),
+  },
+]
+
 
 const FooterTop = () => {
   return (
