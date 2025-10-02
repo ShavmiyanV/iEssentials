afc77b4d146e3d0ae7e3bc13d8da585368c5f1f7
Shavmiyan_V <168546643+ShavmiyanV@users.noreply.github.com>
2025-10-03 00:30:53 +0530

    Update data.tsx

--- constants/data.ts
+++ constants/data.ts
@@ -1,14 +1,33 @@
-import { title } from "process"
+import { title } from "process";
 
 export type HeaderItem = {
-    title: string;
-    href: string;
+  title: string;
+  href: string;
 };
 
 export const headerData: HeaderItem[] = [
-    { title: "Home", href: "/" },
-    { title: "Shop", href: "/shop" },
-    { title: "Blog", href: "/blog" },
-    //{ title: "Contact", href: "/contact" },
-    { title: "Hot Deal", href: "/deal" },
+  { title: "Home", href: "/" },
+  { title: "Shop", href: "/shop" },
+  { title: "Blog", href: "/blog" },
+  //{ title: "Contact", href: "/contact" },
+  { title: "Hot Deal", href: "/deal" },
+];
+
+export const quickLinksData = [
+  { title: "About Us", href: "/about" },
+  { title: "Contact Us", href: "/contact" },
+  { title: "Terms & Conditions", href: "/terms" },
+  { title: "Privacy & Policy", href: "/privacy" },
+  { title: "FAQs", href: "/faqs" },
+  { title: "Help", href: "/help" },
+];
+
+export const categoriesData = [
+  { title: "Mobiles", href: "/mobiles" },
+  { title: "Appliances", href: "/appliances" },
+  { title: "Smartphones", href: "/smartphones" },
+  { title: "Air Conditioner", href: "/air-conditioner" },
+  { title: "Washing Machine", href: "/washing-machine" },
+  { title: "Kitchen Appliances", href: "/kitchen-appliances" },
+  { title: "Gadget Accessories", href: "/gadget-accessories" },
 ];
