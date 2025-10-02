333ed120a763f8f052948e186ff0f840b9bde3a5
Shavmiyan_V <168546643+ShavmiyanV@users.noreply.github.com>
2025-10-02 21:19:08 +0530

    Update FooterTop.tsx

--- components/FooterTop.tsx
+++ components/FooterTop.tsx
@@ -1,6 +1,5 @@
-import { MapPin } from 'lucide-react';
-import React from 'react'
-
+import { MapPin } from "lucide-react";
+import React from "react";
 
 interface ContactItemData {
   title: string;
@@ -13,16 +12,44 @@ const data: ContactItemData[] = [
     title: "Visit Us",
     subtitle: "New Orlean, USA",
     icon: (
-      <MapPin className='h-6 w-6 text-gray-600 group-hover:text-primary transition-colors'/>
+      <MapPin className="h-6 w-6 text-gray-600 group-hover:text-primary transition-colors" />
     ),
   },
-]
-
+  {
+    title: "Call Us",
+    subtitle: "0768816747",
+    icon: (
+      <MapPin className="h-6 w-6 text-gray-600 group-hover:text-primary transition-colors" />
+    ),
+  },
+  {
+    title: "Working Hours",
+    subtitle: "Mon - Sat: 10:00 AM - 7:00 PM",
+    icon: (
+      <MapPin className="h-6 w-6 text-gray-600 group-hover:text-primary transition-colors" />
+    ),
+  },
+  {
+    title: "Email Us",
+    subtitle: "shavmiyan16@gmail.com",
+    icon: (
+      <MapPin className="h-6 w-6 text-gray-600 group-hover:text-primary transition-colors" />
+    ),
+  },
+];
 
 const FooterTop = () => {
-  return (
-    <div>FooterTop</div>
-  )
+  return <div>
+    {data?.map((item,index) => (
+      <ContactItem/>
+    ))}
+  </div>;
+};
+
+const ContactItem = () => {
+  return <div>
+    <p></p>
+  </div>
 }
 
-export default FooterTop
+export default FooterTop;
