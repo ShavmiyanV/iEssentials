a153349cf0b656481c62c1048837f78c01724e39
Shavmiyan_V <168546643+ShavmiyanV@users.noreply.github.com>
2025-10-04 16:20:40 +0530

    Setup Sanity Completely

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "../globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { ClerkProvider } from "@clerk/nextjs";
@@ -19,15 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en" >
-        <body className="font-poppins antialiased">
-          <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="flex-1">{children}</main>
-            <Footer />
-          </div>
-        </body>
-      </html>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-1">{children}</main>
+        <Footer />
+      </div>
     </ClerkProvider>
   );
 }
---
+++ app/layout.tsx
@@ -0,0 +1,9 @@
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body className="font-poppins antialiased">{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
