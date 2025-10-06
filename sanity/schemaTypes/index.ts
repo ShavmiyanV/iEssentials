a6aae6b9555600a10740bab3519f4f4fa90cbbdf
Shavmiyan_V <168546643+ShavmiyanV@users.noreply.github.com>
2025-10-06 21:03:05 +0530

    Add new schema types to Sanity schema index
    Imported and registered additional schema types including orderType, productType, blockContentType, brandType, blogType, blogCategoryType, authorType, and addressType in the schema index. This expands the available content types for the Sanity backend.

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,6 +1,24 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import { categoryType } from './categoryType'
+import { type SchemaTypeDefinition } from "sanity";
+import { categoryType } from "./categoryType";
+import { orderType } from "./orderType";
+import { productType } from "./productType";
+import { blockContentType } from "./blockContentType";
+import { brandType } from "./brandType";
+import { blogType } from "./blogType";
+import { blogCategoryType } from "./blogCategoryType";
+import { authorType } from "./authorType";
+import { addressType } from "./addressType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [categoryType],
-}
+  types: [
+    categoryType,
+    productType,
+    orderType,
+    blockContentType,
+    brandType,
+    blogType,
+    blogCategoryType,
+    authorType,
+    addressType,
+  ],
+};
