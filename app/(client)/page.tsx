df7c25ff331077024e34c5d0c25887f6f4cd91c6
Shavmiyan_V <168546643+ShavmiyanV@users.noreply.github.com>
2025-10-06 23:10:34 +0530

    Add ProductGrid component and use in client page
    Introduced a new ProductGrid component and rendered it within the client page to display product-related content.

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,12 +1,13 @@
 import Container from "@/components/Container";
 import HomeBanner from "@/components/HomeBanner";
-import { Button } from "@/components/ui/button";
+import ProductGrid from "@/components/ProductGrid";
 import React from "react";
 
 const page = () => {
   return (
     <Container className="">
       <HomeBanner/>
+      <ProductGrid/>
     </Container>
   );
 };
