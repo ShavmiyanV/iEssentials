a56df7166b181976ffda6201c3da80c9c5b88773
Shavmiyan_V <168546643+ShavmiyanV@users.noreply.github.com>
2025-10-07 22:49:59 +0530

    Add product fetching logic to ProductGrid
    Introduces useEffect to fetch products from Sanity based on the selected tab's variant. Handles loading state and error logging during the fetch process.

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -1,20 +1,34 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import HomeTabBar from "./HomeTabBar";
 import { productType } from "@/constants/data";
+import { client } from "@/sanity/lib/client";
 
 const ProductGrid = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [selectedTab, setSelectedTab] = useState(productType[0]?.title || "");
 
-const query = `*[_type == "product" && variant==$variant] | order(name desc) {
+  const query = `*[_type == "product" && variant==$variant] | order(name desc) {
 ..., "categories": categories[]->title
 }`;
 
-const params = {}
-  
+  const params = { variant: selectedTab.toLowerCase() };
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await client.fetch(query, params);
+        console.log(response);
+      } catch (error) {
+        console.error("Product fetching Error:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  });
   return (
     <div>
       <HomeTabBar selectedTab={selectedTab} onTabSelect={setSelectedTab} />
