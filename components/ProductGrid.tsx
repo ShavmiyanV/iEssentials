cb2323e3aecb4f5117e700faa37d06a8122ba632
Shavmiyan_V <168546643+ShavmiyanV@users.noreply.github.com>
2025-10-08 22:38:29 +0530

    Improve ProductGrid loading and empty states
    Enhanced the loading spinner with size and animation classes. Updated the UI to display 'No Product' when there are no products, and added a placeholder for product display.

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -37,12 +37,14 @@ const ProductGrid = () => {
       {loading ? (
         <div className="flex flex-col items-center justify-center py-10 min-h-80 gap-4 bg-gray-100 w-full mt-10">
           <div className="space-x-2 flex items-center text-blue-600">
-            <Loader2 />
+            <Loader2 className="w-5 h-6 animate-spin" />
             <span>Product is loading...</span>
           </div>
         </div>
+      ) : products?.length ? (
+        <>Product</>
       ) : (
-        <>products</>
+        <>No Product</>
       )}
     </div>
   );
