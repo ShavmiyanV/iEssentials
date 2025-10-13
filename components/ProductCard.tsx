92be0af106ac292f52ad68e0a6a852005f91210f
Shavmiyan_V <168546643+ShavmiyanV@users.noreply.github.com>
2025-10-13 14:10:16 +0530

    Add badges for new and hot products in ProductCard
    Introduced a 'New Arrival' badge for products with status 'new' and enhanced the 'hot' status by displaying a flame icon using lucide-react. This improves product visibility and user experience on the product card.

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -3,6 +3,7 @@ import { urlFor } from "@/sanity/lib/image";
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { Flame } from "lucide-react";
 
 const ProductCard = ({ product }: { product: Product }) => {
   return (
@@ -22,11 +23,22 @@ const ProductCard = ({ product }: { product: Product }) => {
             Sale!
           </p>
         )}
+        {product?.status === "new" && (
+          <p className="absolute top-2 left-2 z-10 text-xs border border-darkColor/50 px-2 rounded-full group-hover:border-shop_light_green group-hover:text-shop_light_green hoverEffect">
+            New Arrival
+          </p>
+        )}
         {product?.status === "hot" && (
           <Link
             href={"/deal"}
             className="absolute top-2 left-2 z-10 border border-shop_orange/50 p-1 rounded-full group-hover:border-shop_orange hover:text-shop_dark_green hoverEffect"
-          ></Link>
+          >
+            <Flame
+              size={18}
+              fill="#fb6c08"
+              className="text-shop_orange/50 group-hover:text-shop_orange hoverEffect"
+            />
+          </Link>
         )}
       </div>
       <div className="p-3">Product Details</div>
