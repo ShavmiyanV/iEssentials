2da061e12e744f5ba67663ec5f4c0a7082d5b5e1
Shavmiyan_V <168546643+ShavmiyanV@users.noreply.github.com>
2025-10-19 14:38:06 +0530

    Add configurable PriceFormatter component
    Refactored PriceFormatter to accept amount and className as props, format the price as USD currency, and render as a styled span. This improves reusability and allows for custom styling.

--- components/PriceFormatter.tsx
+++ components/PriceFormatter.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { cn } from "@/lib/utils";
 
-const PriceFormatter = () => {
-  return (
-    <div>PriceFormatter</div>
-  )
+interface Props {
+  amount: number | undefined;
+  className?: string;
 }
 
-export default PriceFormatter
+const PriceFormatter = ({ amount, className }: Props) => {
+  const formattedPrice = new Number(amount).toLocaleString("en-US", {
+    currency: "USD",
+    style: "currency",
+    minimumFractionDigits: 2,
+  });
+  return (
+    <span className={cn("text-sm font-semibold text-darkColor", className)}>
+      {formattedPrice}
+    </span>
+  );
+};
+
+export default PriceFormatter;
