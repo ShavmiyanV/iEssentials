49532fab7d24c1cafb4b89f0cefb833b92fe8e6c
Shavmiyan_V <168546643+ShavmiyanV@users.noreply.github.com>
2025-10-20 19:44:54 +0530

    Add Title component and update HomeCategories UI
    Introduced a reusable Title component for section headings. Updated HomeCategories to use Title and improved its styling with Tailwind classes.

--- components/HomeCategories.tsx
+++ components/HomeCategories.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Title from './Title'
+
 
 const HomeCategories = () => {
   return (
-    <div>HomeCategories</div>
+    <div className='bg-white border border-shop_light_green/20 my-10 md:my-20 p-5 lg:p-7 rounded-md'>
+        <Title className="border-b pb-3">Popular Categories</Title>
+    </div>
   )
 }
 
