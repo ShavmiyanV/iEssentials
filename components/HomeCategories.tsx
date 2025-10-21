f7250965e229de0c3c4cbcb29c4d45ff018b2b8e
Shavmiyan_V <168546643+ShavmiyanV@users.noreply.github.com>
2025-10-21 20:13:50 +0530

    Add dynamic category rendering to HomeCategories
    Updated HomeCategories component to accept a categories prop and render category images dynamically using Next.js Image and Sanity image utilities.

--- components/HomeCategories.tsx
+++ components/HomeCategories.tsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import Title from './Title'
+import React from "react";
+import Image from "next/image";
+import Title from "./Title";
+import { Category } from "@/sanity.types";
+import { urlFor } from "@/sanity/lib/image";
 
-
-const HomeCategories = () => {
+const HomeCategories = ({ categories }: { categories: Category[] }) => {
   return (
-    <div className='bg-white border border-shop_light_green/20 my-10 md:my-20 p-5 lg:p-7 rounded-md'>
-        <Title className="border-b pb-3">Popular Categories</Title>
+    <div className="bg-white border border-shop_light_green/20 my-10 md:my-20 p-5 lg:p-7 rounded-md">
+      <Title className="border-b pb-3">Popular Categories</Title>
+      <div>
+        {categories?.map((category) => (
+          <div key={category?._id}>
+            {category?.image && (
+              <Image
+                src={urlFor(category?.image).url()}
+                alt="categoryImage"
+                width={500}
+                height={500}
+                className="w-full h-full object-contain group-hover:scale-110 hoverEffect"
+              />
+            )}
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeCategories
+export default HomeCategories;
