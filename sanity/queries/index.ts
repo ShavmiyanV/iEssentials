b988d6b7cf8ab5249d0c8798e9a8ea1bc43fe98a
Shavmiyan_V <168546643+ShavmiyanV@users.noreply.github.com>
2025-10-21 19:53:30 +0530

    Add product count to category query
    Updated the getCategories function to include a 'productCount' field in the returned category objects, counting the number of products referencing each category. This provides more detailed category data for consumers of the query.

--- sanity/queries/index.ts
+++ sanity/queries/index.ts
@@ -1,9 +1,14 @@
 const getCategories = async (quantity?: number) => {
-    try {
-        const query = quantity 
-        ? `*[_type == 'category'] | order (name asc) [0...$quantity]`
-    } catch (error) {
-        console.log("Error fetching categories:", error);
-        return [];
-    }
-} 
+  try {
+    const query = quantity
+      ? `*[_type == 'category'] | order (name asc) [0...$quantity] {
+        ..., 
+        "productCount" : count (*[_type == "product" && references (^._id)])}`
+      : `*[_type == 'category'] | order(name asc) {
+        ..., 
+        "productCount": count(*[_type == "product" && references (^._id)])}`;
+  } catch (error) {
+    console.log("Error fetching categories:", error);
+    return [];
+  }
+};
