c596d8dd3df1ad6fc1fbf14b7d9eeddf76abe414
Shavmiyan_V <168546643+ShavmiyanV@users.noreply.github.com>
2025-10-22 22:43:53 +0530

    Enhance ShopByBrands component layout and content
    Replaces placeholder content with a styled section including a title and a 'View all' link. Adds layout structure and imports necessary components for improved UI.

--- components/ShopByBrands.tsx
+++ components/ShopByBrands.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import Title from "./Title";
+import Link from "next/link";
 
 const ShopByBrands = () => {
   return (
-    <div>ShopByBrands</div>
-  )
-}
+    <div className="mb-10 lg:pb-20 bg-shop_light_bg p-5 lg:p-7 rounded-md">
+      <div className="flex items-center gap-5 justify-between mb-10">
+        <Title className="text-2xl">Shop By Brands</Title>
+        <Link
+          href={"/shop"}
+          className="text-sm font-semibold tracking-wide hover:text-shop_btn_dark_green hoverEffect"
+        >
+          View all
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default ShopByBrands
+export default ShopByBrands;
