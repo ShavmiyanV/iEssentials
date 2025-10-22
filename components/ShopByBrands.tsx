08eebd63acf5f9fe26b292d1f5213610aad2f6e1
Shavmiyan_V <168546643+ShavmiyanV@users.noreply.github.com>
2025-10-22 22:49:53 +0530

    Make ShopByBrands component async and fetch brands
    Updated ShopByBrands to be an async component and fetch brand data using getBrands(). Also removed unnecessary text size class from the Title component.

--- components/ShopByBrands.tsx
+++ components/ShopByBrands.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import Title from "./Title";
 import Link from "next/link";
 
-const ShopByBrands = () => {
+const ShopByBrands = async () => { 
+    const brands = await getBrands();
   return (
     <div className="mb-10 lg:pb-20 bg-shop_light_bg p-5 lg:p-7 rounded-md">
       <div className="flex items-center gap-5 justify-between mb-10">
-        <Title className="text-2xl">Shop By Brands</Title>
+        <Title>Shop By Brands</Title>
         <Link
           href={"/shop"}
           className="text-sm font-semibold tracking-wide hover:text-shop_btn_dark_green hoverEffect"
