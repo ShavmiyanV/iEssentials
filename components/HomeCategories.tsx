03d21b948debb71e41dbb1f7c708d181a702e0f9
Shavmiyan_V <168546643+ShavmiyanV@users.noreply.github.com>
2025-10-22 22:22:05 +0530

    Display category title and product count on Home
    Added a section to show each category's title and the number of available items in HomeCategories. This improves visibility of category details for users.

--- components/HomeCategories.tsx
+++ components/HomeCategories.tsx
@@ -28,6 +28,13 @@ const HomeCategories = ({ categories }: { categories: Category[] }) => {
                 </Link>
               </div>
             )}
+            <div className="space-y-1">
+              <h3 className="text-base font-semibold">{category?.title}</h3>
+              <p className="text-sm">
+                <span className="font-bold text-shop_dark_green">{`(${category?.productCount})`}</span>{" "}
+                Items Available
+              </p>
+            </div>
           </div>
         ))}
       </div>
