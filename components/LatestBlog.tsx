6978a13ddc7d64d9482ef9d2aaa2fc3cb5e318c8
Shavmiyan_V <168546643+ShavmiyanV@users.noreply.github.com>
2025-10-26 19:21:44 +0530

    Wrap blog images with links in LatestBlog
    Updated the LatestBlog component to wrap each blog's main image in a Link to the blog's detail page. Also improved the blog grid layout for better responsiveness.

--- components/LatestBlog.tsx
+++ components/LatestBlog.tsx
@@ -3,23 +3,26 @@ import Title from "./Title";
 import { getLatestBlogs } from "@/sanity/queries";
 import Image from "next/image";
 import { urlFor } from "@/sanity/lib/image";
+import Link from "next/link";
 
 const LatestBlog = async () => {
   const blogs = await getLatestBlogs();
   return (
     <div className="mb-10 lg:mb-20">
       <Title>Latest Blog</Title>
-      <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mt-5">
         {blogs?.map((blog) => (
           <div key={blog?._id}>
             {blog?.mainImage && (
-              <Image
-                src={urlFor(blog.mainImage!).url()}
-                alt="blogImage"
-                width={500}
-                height={300}
-                className="w-full max-h-80 object-cover"
-              />
+              <Link href={`/blog/${blog?.slug?.current}`}>
+                <Image
+                  src={urlFor(blog.mainImage!).url()}
+                  alt="blogImage"
+                  width={500}
+                  height={300}
+                  className="w-full max-h-80 object-cover"
+                />
+              </Link>
             )}
           </div>
         ))}
