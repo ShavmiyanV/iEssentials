87b22074b5f449223e5806a8664e6c832ce31c6b
Shavmiyan_V <168546643+ShavmiyanV@users.noreply.github.com>
2025-10-27 23:14:34 +0530

    Add styling to blog card container
    Added 'rounded-lg' and 'overflow-hidden' classes to the blog card container in LatestBlog.tsx for improved appearance and consistent styling.

--- components/LatestBlog.tsx
+++ components/LatestBlog.tsx
@@ -14,7 +14,7 @@ const LatestBlog = async () => {
       <Title>Latest Blog</Title>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mt-5">
         {blogs?.map((blog) => (
-          <div key={blog?._id}>
+          <div key={blog?._id} className="rounded-lg overflow-hidden">
             {blog?.mainImage && (
               <Link href={`/blog/${blog?.slug?.current}`}>
                 <Image
