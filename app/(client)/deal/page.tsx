d92a79c63aa3f13ee13fdf72f1b5b41242f7baca
Shavmiyan_V <168546643+ShavmiyanV@users.noreply.github.com>
2025-10-31 01:44:55 +0530

    Display hot deal products on deal page
    Added a grid of ProductCard components to the deal page to show products with 'hot' status. Introduced the DEAL_PRODUCTSResult type in sanity.types.ts to support the new product query for hot deals.

--- app/(client)/deal/page.tsx
+++ app/(client)/deal/page.tsx
@@ -1,3 +1,4 @@
+import ProductCard from "@/components/ProductCard";
 import Title from "@/components/Title";
 import { getDealProducts } from "@/sanity/queries";
 import { Container } from "lucide-react";
@@ -11,6 +12,11 @@ const DealPage = async () => {
         <Title className="mb-5 underline underline-offset-4 decoration-[1px] text-base uppercase tracking-wide">
           Hot Deals of the Week
         </Title>
+        <div className="grid grid-cols-2 gap-2.5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 ">
+          {products?.map((product) => (
+            <ProductCard key={product?._id} product={product} />
+          ))}
+        </div>
       </Container>
     </div>
   );
