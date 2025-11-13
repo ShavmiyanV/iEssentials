410edbd6025f880d7b07621e539b7bf53b2ae103
Shavmiyan_V <168546643+ShavmiyanV@users.noreply.github.com>
2025-11-13 23:07:39 +0530

    Add category page layout and fetch categories
    Introduces async CategoryPage component that fetches categories and displays a title using the Container and Title components. Sets up the basic structure for category-based product listing.

--- app/(client)/category/[slug]/page.tsx
+++ app/(client)/category/[slug]/page.tsx
@@ -1,8 +1,16 @@
+import Title from '@/components/Title';
+import { getCategories } from '@/sanity/queries'
+import { Container } from 'lucide-react';
 import React from 'react'
 
-const CategoryPage = () => {
+const CategoryPage = async({params}:{params:Promise<{slug: string}>}) => { 
+  const categories = await getCategories();
   return (
-    <div>CategoryPage</div>
+    <div className='py-10'>
+      <Container>
+        <Title>Products by Category</Title>
+      </Container>
+    </div>
   )
 }
 
