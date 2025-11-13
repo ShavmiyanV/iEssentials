7f7b1ca2ee5b4ff08009471eed826559dbcdbef6
Shavmiyan_V <168546643+ShavmiyanV@users.noreply.github.com>
2025-11-13 23:09:54 +0530

    Display selected category in page title
    Updates the category page to show the selected category slug in the title, improving clarity for users viewing products by category. Also refactors formatting and ensures proper extraction of the slug from params.

--- app/(client)/category/[slug]/page.tsx
+++ app/(client)/category/[slug]/page.tsx
@@ -1,17 +1,27 @@
-import Title from '@/components/Title';
-import { getCategories } from '@/sanity/queries'
-import { Container } from 'lucide-react';
-import React from 'react'
+import Title from "@/components/Title";
+import { getCategories } from "@/sanity/queries";
+import { Container } from "lucide-react";
+import React from "react";
 
-const CategoryPage = async({params}:{params:Promise<{slug: string}>}) => { 
+const CategoryPage = async ({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) => {
   const categories = await getCategories();
+  const { slug } = await params;
   return (
-    <div className='py-10'>
+    <div className="py-10">
       <Container>
-        <Title>Products by Category</Title>
+        <Title>
+          Products by Category:{" "}
+          <span className="font-bold text-green-600 capitalize tracking-wide">
+            {slug && slug}
+          </span>
+        </Title>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default CategoryPage
+export default CategoryPage;
