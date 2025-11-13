9f27101c8010ee8880c0895f767dc817f702afeb
Shavmiyan_V <168546643+ShavmiyanV@users.noreply.github.com>
2025-11-13 23:17:54 +0530

    Add state and props to CategoryProducts component
    CategoryProducts now accepts categories and slug as props, manages currentSlug, products, and loading state, and uses Next.js router. This sets up the component for future functionality related to category-based product display.

--- components/CategoryProducts.tsx
+++ components/CategoryProducts.tsx
@@ -1,12 +1,17 @@
 "use client";
 import { Category } from "@/sanity.types";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 interface Props {
   categories: Category[];
   slug: string;
 }
 
-const CategoryProducts = () => {
+const CategoryProducts = ({ categories, slug }: Props) => {
+  const [currentSlug, setCurrentSlug] = useState(slug);
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
   return <div>CategoryProducts</div>;
 };
 
